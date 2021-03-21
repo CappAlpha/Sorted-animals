197589fca11bbe31dac6728d57fe0c2c1ce9c0a0
Egor <egor.lapin.workspace@gmail.com>
2021-03-21 15:36:40 +0300

    yes

--- Prac/Program.cs
+++ Prac/Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
-
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
 namespace Prac
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-             Console.WriteLine("\nYO!");
+            const double pi = 3.14;
+            double r;
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(pi * r * r);
+            Console.WriteLine("kek");
         }
     }
 }
